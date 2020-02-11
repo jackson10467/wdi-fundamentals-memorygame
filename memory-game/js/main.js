35f34c76f5c77a7d9349ce810e26340f021926fa
Jackson Vu <jacksonvu@Jacksons-MBP.home>
2020-02-11 15:13:27 -0500

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,9 +1,23 @@
 console.log("up and running!");
-let card1 = "queen";
-let card2 = "queen";
-let card3 = "king";
-let card4 = "king";
+let cards = ['queen','queen','king','king'];
+let cardsPlay = [];
+
+let card1 = cards[0];
+cardsPlay.push(card1);
+
+let card2 = cards[2];
+cardsPlay.push(card2);
+
 
 
 console.log("User Flipped " + card1);
-console.log("User Flipped " + card4);
+console.log("User Flipped " + card2);
+
+if(cardsPlay.length === 2){
+	if(cardsPlay[0]===cardsPlay[1]){
+		alert("You found a match!");
+	}
+	else{
+		alert('try again');
+}
+}
