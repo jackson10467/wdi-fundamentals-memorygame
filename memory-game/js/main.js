7071c3b2c8b1057488e26c7c3b7ea7c0e0d1953a
Jackson Vu <jacksonvu@Jacksons-MBP.home>
2020-02-10 22:46:17 -0500

    created game logic

---
+++ memory-game/js/main.js
@@ -0,0 +1,9 @@
+console.log("up and running!");
+let card1 = "queen";
+let card2 = "queen";
+let card3 = "king";
+let card4 = "king";
+
+
+console.log("User Flipped " + card1);
+console.log("User Flipped " + card4);
